0293bc92fae5dd073b246c9e6eac6c94dbc1ff67
Adam Amer <adamamer1801@gmail.com>
2022-07-10 21:01:15 +0300

    Replaced type of project meta from JSON to string to avoid parse errors

--- src/database/utils/Project.ts
+++ src/database/utils/Project.ts
@@ -10,15 +10,22 @@ async function getProjectByName(name: string): Promise<Project | null> {
         .catch((err: any) => {errlog("Error getting project by name", "database");throw err;});
 
     if (ProjectObject === null) return null;
-    else return ProjectObject;
+    ProjectObject.project_meta = JSON.parse(ProjectObject.project_meta);
+    return ProjectObject;
 }
 
 async function getProjects(limit?: number): Promise<Project[]> {
-    const ProjectObjects: Project[] = await db.get().getRepository(Project)
+    const _ProjectObjects: Project[] = await db.get().getRepository(Project)
         .createQueryBuilder("project")
         .limit(limit || 10000)
         .getMany()
         .catch((err: any) => {errlog("Error getting projects", "database");throw err;});
+    
+    const ProjectObjects: Project[] = [];
+    for (const ProjectObject of _ProjectObjects) {
+        ProjectObject.project_meta = JSON.parse(ProjectObject.project_meta);
+        ProjectObjects.push(ProjectObject);
+    }
 
     return ProjectObjects;
 } 
@@ -26,7 +33,7 @@ async function getProjects(limit?: number): Promise<Project[]> {
 async function editProject(name: string, meta?: Object, image?: string): Promise<Project | null> {
     const ProjectObject: Project | null = await getProjectByName(name);
     if (ProjectObject === null) return null;
-    if (meta !== undefined) ProjectObject.project_meta = meta;
+    if (meta !== undefined) ProjectObject.project_meta = meta.toString();
     if (image !== undefined) ProjectObject.project_image = image;
     await db.get().getRepository(Project).save(ProjectObject).catch((err: any) => {errlog("Error editing project", "database");throw err;});
     return ProjectObject;
@@ -35,7 +42,7 @@ async function editProject(name: string, meta?: Object, image?: string): Promise
 async function newProject(name: string, meta: Object, image: string): Promise<Project> {
     const ProjectObject: Project = new Project();
     ProjectObject.project_name = name;
-    ProjectObject.project_meta = meta;
+    ProjectObject.project_meta = meta.toString();
     ProjectObject.project_image = image;
     await db.get().getRepository(Project).save(ProjectObject).catch((err: any) => {errlog("Error creating new project", "database");throw err;});
     return ProjectObject;
